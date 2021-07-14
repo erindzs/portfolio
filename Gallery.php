14874a51a3aeca8c25158b0c05957b9ff7ef1abe
erindzs <edijsrindzs@gmail.com>
2021-07-14 23:09:59 +0300

    added script.js and lightbox for galley

--- Gallery.php
+++ Gallery.php
@@ -1,5 +1,3 @@
-
-
 <!doctype html>
 <html class="gallerybg">
 <meta charset="utf-8"/>
@@ -26,27 +24,92 @@
     <a href="ContactMe.php">CONTACT</a>
   </div>
 </div>
-  
-<div class="gallery">
-  <div class="gallery__item gallery__item--1">
-    <img src="img/1.png" class="gallery__img" alt="1">
-  </div>
-  <div class="gallery__item gallery__item--2">
-    <img src="img/2.png" class="gallery__img" alt="2">
-  </div>
-  <div class="gallery__item gallery__item--3">
-    <img src="img/3.png" class="gallery__img" alt="3">
-  </div>
-  <div class="gallery__item gallery__item--4">
-    <img src="img/4.png" class="gallery__img" alt="4">
-  </div>
-  <div class="gallery__item gallery__item--5">
-    <img src="img/5.png" class="gallery__img" alt="5">
-  </div>
-  <div class="gallery__item gallery__item--6">
-    <img src="img/6.png" class="gallery__img" alt="6">
+<div class="row"> 
+    <div class="gallery">
+    <div class="gallery__item gallery__item--1 column" style="width:100%" onclick="openModal();currentSlide(1)" class="hover-shadow cursor">
+      <img src="img/1.png" class="gallery__img" alt="1">
+    </div>
+    <div class="gallery__item gallery__item--2 column" style="width:100%" onclick="openModal();currentSlide(2)" class="hover-shadow cursor">
+      <img src="img/2.png" class="gallery__img" alt="2">
+    </div>
+    <div class="gallery__item gallery__item--3 column" style="width:100%" onclick="openModal();currentSlide(3)" class="hover-shadow cursor">
+      <img src="img/3.png" class="gallery__img" alt="3">
+    </div>
+    <div class="gallery__item gallery__item--4 column" style="width:100%" onclick="openModal();currentSlide(4)" class="hover-shadow cursor">
+      <img src="img/4.png" class="gallery__img" alt="4">
+    </div>
+    <div class="gallery__item gallery__item--5 column" style="width:100%" onclick="openModal();currentSlide(5)" class="hover-shadow cursor">
+      <img src="img/5.png" class="gallery__img" alt="5">
+    </div>
+    <div class="gallery__item gallery__item--6 column" style="width:100%" onclick="openModal();currentSlide(6)" class="hover-shadow cursor">
+      <img src="img/6.png" class="gallery__img" alt="6">
+    </div>
   </div>
 </div>
+<div id="myModal" class="modal">
+  <span class="close cursor" onclick="closeModal()">&times;</span>
+  <div class="modal-content">
+
+    <div class="mySlides">
+      <div class="numbertext">1 / 6</div>
+      <img src="img/1.png" style="width:100%">
+    </div>
+
+    <div class="mySlides">
+      <div class="numbertext">2 / 6</div>
+      <img src="img/2.png" style="width:100%">
+    </div>
+
+    <div class="mySlides">
+      <div class="numbertext">3 / 6</div>
+      <img src="img/3.png" style="width:100%">
+    </div>
+    
+    <div class="mySlides">
+      <div class="numbertext">4 / 6</div>
+      <img src="img/4.png" style="width:100%">
+    </div>
+    <div class="mySlides">
+      <div class="numbertext">5/ 6</div>
+      <img src="img/5.png" style="width:100%">
+    </div>
+    <div class="mySlides">
+      <div class="numbertext">6/ 6</div>
+      <img src="img/6.png" style="width:100%">
+    </div>
+    
+    <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+    <a class="next" onclick="plusSlides(1)">&#10095;</a>
+
+    <div class="caption-container">
+      <p id="caption"></p>
+    </div>
+
+
+    <div class="column">
+      <img class="demo cursor" src="img/1.png" style="width:100%%" onclick="currentSlide(1)" alt="1">
+    </div>
+    <div class="column">
+      <img class="demo cursor" src="img/2.png" style="width:100%%" onclick="currentSlide(2)" alt="2">
+    </div>
+    <div class="column">
+      <img class="demo cursor" src="img/3.png" style="width:100%%" onclick="currentSlide(3)" alt="3">
+    </div>
+    <div class="column">
+      <img class="demo cursor" src="img/4.png" style="width:100%%" onclick="currentSlide(4)" alt="4">
+    </div>
+    <div class="column">
+      <img class="demo cursor" src="img/5.png" style="width:100%%" onclick="currentSlide(4)" alt="5">
+    </div>
+    <div class="column">
+      <img class="demo cursor" src="img/6.png" style="width:100%%" onclick="currentSlide(4)" alt="6">
+    </div>
+  </div>
+</div>
+
+
+
+<script src="script.js"></script>
 
 <?php
 
